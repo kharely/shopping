9b8d1d73bdbe41645969f406a13a25bab74f4839
kharely <kadelyta1996@gmail.com>
2019-09-30 20:05:43 -0500

    Exporting items

--- database/items.js
+++ database/items.js
@@ -1,29 +1,6 @@
 const db = require('./connection')
-
-
-let objs =[
-  {
-    id:1,
-    CODE: 'PANTS',
-    NAME: 'Pants',
-    PRICE: 5.00
-  },
-  {
-    id:2,
-    CODE: 'TSHIRT',
-    NAME: 'T-shirt',
-    PRICE: 20.00
-  },
-  {
-    id:3,
-    CODE: 'HAT',
-    NAME: 'Hat',
-    PRICE: 7.50
-  }
-]
-
-
 const items = db.get('items')
+const objs = require('./shopping_list')
 
 function getAll(){
  let map1 = new Map()
@@ -35,7 +12,7 @@ function getAll(){
 }
 
 function createItems(){
-  items.insert(objs)
+  items.insert(objs.options())
 }
 
 function calculateItem(quantity, type){
---
+++ database/shopping_list.js
@@ -0,0 +1,26 @@
+function options(){
+return [
+  {
+    id:1,
+    CODE: 'PANTS',
+    NAME: 'Pants',
+    PRICE: 5.00
+  },
+  {
+    id:2,
+    CODE: 'TSHIRT',
+    NAME: 'T-shirt',
+    PRICE: 20.00
+  },
+  {
+    id:3,
+    CODE: 'HAT',
+    NAME: 'Hat',
+    PRICE: 7.50
+  }
+]
+}
+
+module.exports= {
+  options
+}
